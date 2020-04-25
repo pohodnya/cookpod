6a1da05565ef0c25faf926c8c25d1129c78ccce7
alekseypokhodnya <pohodnyaa@gmail.com>
2020-04-25 15:07:20 +0700

    Add bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,7 @@ import css from "../css/app.css"
 // Import dependencies
 //
 import "phoenix_html"
+import "bootstrap/dist/js/bootstrap.min"
 
 // Import local files
 //
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Cookpod.MixProject do
       elixirc_options: [warnings_as_error: true],
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: ["coveralls.json": :test]
     ]
   end
 
@@ -47,7 +47,8 @@ defmodule Cookpod.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_slime, "~> 0.13.1"}
     ]
   end
 
